c993a74c1314fef0b83110a2e3d00fd855dfd91e
Lionel Ritchie <lionelrtchieee@gmail.com>
2021-07-02 20:16:22 +0700

    init scene

---
+++ scripts/globals/global.js
@@ -0,0 +1,8 @@
+const GLOBALS = {
+  ASPECT_RATIO: window.innerWidth / window.innerHeight,
+  ORBIT_FOV: 75,
+  NEAR: 0.1,
+  FAR: 1000
+}
+
+export default GLOBALS;
--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,34 @@
 import * as THREE from './threejs/build/three.module.js';
+import GLOBALS from './globals/global.js';
 
 const load = () => {
   console.log("Loaded succesfully");
+  
+  const {scene, orbitCamera, renderer} = init();
+
+  const animate = () => {
+    requestAnimationFrame(animate);
+    renderer.render(scene, orbitCamera);
+  }
+
+  animate();
 }
 
+const init = () => {
+  const {ASPECT_RATIO, NEAR, FAR, ORBIT_FOV} = GLOBALS;
+  const orbitCamera = new THREE.PerspectiveCamera(ORBIT_FOV, ASPECT_RATIO, NEAR, FAR); 
+  const scene = new THREE.Scene();
+  const renderer = initRenderer();
+
+  return {scene, orbitCamera, renderer};
+}
+
+const initRenderer = () => {
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+  return renderer;
+}
 
 load();
